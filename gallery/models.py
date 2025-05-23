3fb872e3d627bc2f25ce80efc9d35dc5200e8cee
bubron <143940924+bubron@users.noreply.github.com>
2025-05-23 09:32:41 +0300

    Added Category model

--- gallery/models.py
+++ gallery/models.py
@@ -1,1 +1,12 @@
 from django.db import models
+from django.utils import timezone
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Назва категорії")
+
+    class Meta:
+        verbose_name = "Категорія"
+        verbose_name_plural = "Категорії"
+
+    def __str__(self):
+        return self.name
