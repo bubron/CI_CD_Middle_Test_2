9f240e842c066ae0c6cf1361968b0b10400bf7c1
bubron <143940924+bubron@users.noreply.github.com>
2025-05-23 09:39:41 +0300

    Adde setUp test

--- gallery/tests.py
+++ gallery/tests.py
@@ -8,3 +8,22 @@ class CategoryModelTest(TestCase):
         category = Category.objects.create(name="Природа")
         self.assertEqual(str(category), "Природа")
         self.assertEqual(Category.objects.count(), 1)
+
+class ImageModelTest(TestCase):
+    def setUp(self):
+        self.category1 = Category.objects.create(name="Природа")
+        self.category2 = Category.objects.create(name="Міста")
+        
+        test_image = SimpleUploadedFile(
+            name='test_image.jpg',
+            content=b'',
+            content_type='image/jpeg'
+        )
+        
+        self.image = Image.objects.create(
+            title="Тестове зображення",
+            image=test_image,
+            created_date=timezone.now().date(),
+            age_limit=12
+        )
+        self.image.categories.add(self.category1, self.category2)
