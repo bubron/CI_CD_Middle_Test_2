340014bebd8b6480f5a2de9cc69dbd84faa51651
bubron <143940924+bubron@users.noreply.github.com>
2025-05-23 09:37:59 +0300

    Added test_category_creation test

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from django.core.files.uploadedfile import SimpleUploadedFile
+from .models import Category, Image
+from django.utils import timezone
 
-# Create your tests here.
+class CategoryModelTest(TestCase):
+    def test_category_creation(self):
+        category = Category.objects.create(name="Природа")
+        self.assertEqual(str(category), "Природа")
+        self.assertEqual(Category.objects.count(), 1)
