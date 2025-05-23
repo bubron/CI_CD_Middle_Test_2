5fc6d8e38153d615187a41b2b289732175d0d453
bubron <143940924+bubron@users.noreply.github.com>
2025-05-23 09:35:27 +0300

    Updated admin.py

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,13 +1,11 @@
 from django.contrib import admin
-from .models import Image, Category
-
-
-@admin.register(Image)
-class ImageAdmin(admin.ModelAdmin):
-    list_display = ('title', 'image',)
-    filter_horizontal = ('categories',)
-
+from .models import Category, Image
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name',)
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_date', 'age_limit')
+    filter_horizontal = ('categories',)
