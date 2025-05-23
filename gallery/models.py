02e8e2c3324ee3ca691f470a491fe2e64614be65
bubron <143940924+bubron@users.noreply.github.com>
2025-05-23 09:33:52 +0300

    Added Image model

--- gallery/models.py
+++ gallery/models.py
@@ -10,3 +10,17 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+class Image(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Заголовок")
+    image = models.ImageField(upload_to='images/', verbose_name="Зображення")
+    categories = models.ManyToManyField(Category, verbose_name="Категорії")
+    created_date = models.DateField(default=timezone.now, verbose_name="Дата створення")
+    age_limit = models.PositiveIntegerField(default=0, verbose_name="Вікове обмеження")
+
+    class Meta:
+        verbose_name = "Зображення"
+        verbose_name_plural = "Зображення"
+
+    def __str__(self):
+        return self.title
